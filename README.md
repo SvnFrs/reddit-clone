ad43eadaed06ee8c8b20753030e842fd22fc833d
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-10-11 17:46:16 +0700

    Basically have the skeleton for this project, hope I can continue doing this

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reddit clone 
+## Description
+This is one of two, or many projects which I aim to create to level up my skills in web development. This project is a clone of the popular social media site Reddit. I will be using both good old HTML, CSS, JS and ReactJS to recreate this site.
---
+++ basic/js/script.js
@@ -0,0 +1,9 @@
+function dropdownEnableNav() {
+    document.getElementById("dropdown-content").classList.toggle("show");
+}
+function dropdownEnableSort() {
+    document.getElementById("dropdown-sort").classList.toggle("show");
+}
+function dropdownEnableMode(){
+    document.getElementById("dropdown-mode").classList.toggle("show");
+}
